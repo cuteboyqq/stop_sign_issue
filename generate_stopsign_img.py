2a0237ca8739ec6d78bb9c52cf9277c48b191d57
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-08-28 14:57:00 +0800

    Add files via upload

---
+++ generate_stopsign_img.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import shutil
+import cv2
+
+
+def Generate_StopSign_Img(roi_dir=None,
+                          img_dir=None,
+                          label_dir=None,
+                          save_dir=None):
+    
+ return NotImplementedError
+
+
+def get_args():
+   import argparse
+   
+
+
+if __name__=="__main__":
+
+    Generate_StopSign_Img(roi_dir,
+                          img_dir,
+                          label_dir,
+                          save_dir):
